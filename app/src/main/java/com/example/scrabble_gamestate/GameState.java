69d477e97569676a80d1fa0a47169a7371c8aee0
Sarah <bunger20@up.edu>
2019-02-21 12:57:35 -0800

    Some instance vars added

--- app/src/main/java/com/example/scrabble_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble_gamestate/GameState.java
@@ -20,4 +20,42 @@ public class GameState {
      * You may need to create additional classes to describe specific elements of the game state (e.g., a playing card, a pawn, a tile, etc.)
      */
 
+    final int A_VAL = 1;
+    final int B_VAL = 1;
+    final int C_VAL = 1;
+    final int D_VAL = 1;
+    final int E_VAL = 1;
+    final int F_VAL = 1;
+    final int G_VAL = 1;
+    final int H_VAL = 1;
+    final int I_VAL = 1;
+    final int J_VAL = 1;
+    final int K_VAL = 1;
+    final int L_VAL = 1;
+    final int M_VAL = 1;
+    final int N_VAL = 1;
+    final int O_VAL = 1;
+    final int P_VAL = 1;
+    final int Q_VAL = 1;
+    final int R_VAL = 1;
+    final int S_VAL = 1;
+    final int T_VAL = 1;
+    final int U_VAL = 1;
+    final int V_VAL = 1;
+    final int W_VAL = 1;
+    final int X_VAL = 1;
+    final int Y_VAL = 1;
+    final int Z_VAL = 1;
+
+
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
+
+
+
+
+
+
+
+
 }
