cddccdd83d4595fd16eaf685332425288f2b869b
Sarah <bunger20@up.edu>
2019-02-21 13:20:28 -0800

    More instance vars added

--- app/src/main/java/com/example/scrabble_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble_gamestate/GameState.java
@@ -1,5 +1,7 @@
 package com.example.scrabble_gamestate;
 
+import java.util.ArrayList;
+
 public class GameState {
 
 
@@ -21,39 +23,46 @@ public class GameState {
      */
 
     final int A_VAL = 1;
-    final int B_VAL = 1;
-    final int C_VAL = 1;
-    final int D_VAL = 1;
+    final int B_VAL = 3;
+    final int C_VAL = 3;
+    final int D_VAL = 2;
     final int E_VAL = 1;
-    final int F_VAL = 1;
-    final int G_VAL = 1;
-    final int H_VAL = 1;
+    final int F_VAL = 4;
+    final int G_VAL = 2;
+    final int H_VAL = 4;
     final int I_VAL = 1;
-    final int J_VAL = 1;
-    final int K_VAL = 1;
+    final int J_VAL = 8;
+    final int K_VAL = 5;
     final int L_VAL = 1;
-    final int M_VAL = 1;
+    final int M_VAL = 3;
     final int N_VAL = 1;
     final int O_VAL = 1;
-    final int P_VAL = 1;
-    final int Q_VAL = 1;
+    final int P_VAL = 3;
+    final int Q_VAL = 10;
     final int R_VAL = 1;
     final int S_VAL = 1;
     final int T_VAL = 1;
     final int U_VAL = 1;
-    final int V_VAL = 1;
-    final int W_VAL = 1;
-    final int X_VAL = 1;
-    final int Y_VAL = 1;
-    final int Z_VAL = 1;
+    final int V_VAL = 4;
+    final int W_VAL = 4;
+    final int X_VAL = 8;
+    final int Y_VAL = 4;
+    final int Z_VAL = 10;
+    final int BLANK_VAL = 0;
 
 
-    int playerOneScore = 0;
-    int playerTwoScore = 0;
+    int playerOneScore;
+    int playerTwoScore;
 
+    int playerOneId;
+    int playerTwoId;
 
+    ArrayList<Tile> tileBag = new ArrayList<Tile>(98);
+    ArrayList<Tile> hand1 = new ArrayList<Tile>(7);
+    ArrayList<Tile> hand2 = new ArrayList<Tile>(7);
 
-
+    //if true, tile has been played and cannot be moved; if false, tile can be moved
+    boolean tilePlayed;
 
 
 
