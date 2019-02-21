108ef44b7b172a6adf802923048332c1d28de261
Kavya Mandla <mandla21@up.edu>
2019-02-21 12:36:10 -0800

    comments

--- app/src/main/java/com/example/scrabble_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble_gamestate/GameState.java
@@ -2,4 +2,22 @@ package com.example.scrabble_gamestate;
 
 public class GameState {
 
+
+    /*
+     *Information about the resources each player has (e.g., cards, pawns, money)
+     *  - tiles (point value of each tile: int (define as constant))
+     *  - quantity of each tile (ex: 10
+     * Information about the state of any resources (e.g., card is visible, pawn is yellow)
+     *
+     *
+     * Whose turn is it?
+     * Detailed information about shared resources (e.g., the game board, contents of a draw pile)
+     * The visibility of certain information from the perspective of each player
+     * Current score of each player
+     * Current state of a timer
+     * Current state of the dice
+     * What stage of the game you are at (e.g., setup phase, placement phase, buy phase, main play stage, etc.).
+     * You may need to create additional classes to describe specific elements of the game state (e.g., a playing card, a pawn, a tile, etc.)
+     */
+
 }
