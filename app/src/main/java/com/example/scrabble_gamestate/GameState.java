e047f0b7bb4f857267e5233829c43a1bd23c4292
Sarah <bunger20@up.edu>
2019-02-25 18:33:31 -0800

    Constructor completed, though drawCard helper method unfinished. Skeletons added for methods mentioned in actions.txt

--- app/src/main/java/com/example/scrabble_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble_gamestate/GameState.java
@@ -1,6 +1,7 @@
 package com.example.scrabble_gamestate;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameState {
 
@@ -36,18 +37,23 @@ public class GameState {
     //instance variable
     int playerOneScore;
     int playerTwoScore;
+    //int playerThreeScore;
+    //int playerFourScore;
+
 
     int playerOneId;
     int playerTwoId;
 
-    ArrayList<Tile> tileBag = new ArrayList<Tile>(98);
+    ArrayList<Tile> tileBag = new ArrayList<Tile>(100);
     ArrayList<Tile> hand1 = new ArrayList<Tile>(7);
     ArrayList<Tile> hand2 = new ArrayList<Tile>(7);
+    //ArrayList<Tile> hand3 = new ArrayList<Tile>(7);
+    //ArrayList<Tile> hand4 = new ArrayList<Tile>(7);
 
     //if true, tile has been played and cannot be moved; if false, tile can be moved
     boolean tilePlayed;
 
-    //indicates which players turn it is; 1 for player 1, 2 for player 2
+    //indicates which players turn it is; 1 for player 1, 2 for player 2, etc.
     int turn;
 
     /**
@@ -71,15 +77,259 @@ public class GameState {
             drawTile(hand2);
         }
 
+    }//constructor
+
+    /**
+     * makeTileBag
+     * Method to make the bag of tiles for a new game
+     */
+    public void makeTileBag() {
+
+        //blank tiles
+        ///////////////////////////to be added!
+
+        //a tiles
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_2));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_3));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_4));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_5));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_6));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_7));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_8));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_9));
+
+        //b tiles
+        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b));
+        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b_2));
+
+        //c tiles
+        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c));
+        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c_2));
+
+        //d tiles
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_2));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_3));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_4));
+
+        //e tiles
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_2));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_3));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_4));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_5));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_6));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_7));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_8));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_9));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_10));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_11));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_12));
+
+        //f tiles
+        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f));
+        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f_2));
+
+        //g tiles
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g));
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_2));
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_3));
+
+        //h tiles
+        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h));
+        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h_2));
+
+        //i tiles
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_2));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_3));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_4));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_5));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_6));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_7));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_8));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_9));
+
+        //j tile
+        tileBag.add(new Tile(J_VAL, 'j', R.drawable.tile_j));
+
+        //k tile
+        tileBag.add(new Tile(K_VAL, 'k', R.drawable.tile_k));
+
+        //l tiles
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_2));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_3));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_4));
+
+        //m tiles
+        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m));
+        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m_2));
+
+        //n tiles
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_2));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_3));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_4));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_5));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_6));
+
+        //o tiles
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_2));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_3));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_4));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_5));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_6));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_7));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_8));
+
+        //p tiles
+        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p));
+        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p_2));
+
+        //q tile
+        tileBag.add(new Tile(Q_VAL, 'q', R.drawable.tile_q));
+
+        //r tiles
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_2));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_3));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_4));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_5));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_6));
+
+        //s tiles
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_2));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_3));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_4));
+
+        //t tiles
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_2));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_3));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_4));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_5));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_6));
+
+        //u tiles
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_2));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_3));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_4));
+
+        //v tiles
+        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v));
+        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v_2));
+
+        //w tiles
+        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w));
+        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w_2));
+
+        //x tile
+        tileBag.add(new Tile(X_VAL, 'x', R.drawable.tile_x));
+
+        //y tiles
+        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y));
+        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y_2));
+
+        //z tile
+        tileBag.add(new Tile(Z_VAL, 'z', R.drawable.tile_z));
+
+    }//makeTileBag
+
+    /**
+     * Method to shuffle the tiles in the bag
+     */
+    public void shuffleTileBag()
+    {
+        //Collections.shuffle - shuffles an arrayList
+        Collections.shuffle(tileBag);
+
+    }//shuffleTileBag
+
+    /**
+     * Method to add a tile to a specific hand
+     *
+     * @param hand  The hand to add the tile to
+     */
+    public void drawTile(ArrayList<Tile> hand)
+    {
+        hand.add(tileBag.get(0));
+        tileBag.remove(0);
+
+    }//drawTile
+
+    @Override
+    public String toString(){
+
+
+        return null;
     }
 
     /**
-     * makeDeck
-     * Method to make the deck for a new game
+     *
      */
-    public void makeDeck()
-    {
-        deck.add(new Card(0,0,'n',R.drawable.green0));//1
+    public boolean quitGame(){
+        return false;
 
+    }
 
+    /**
+     *
+     */
+    public boolean placeTile(){
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public boolean recallTiles(){
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public boolean playWord(){
+        return false;
+
+    }
+
+    /**
+     *
+     */
+    public boolean skipTurn(){
+        return false;
+
+    }
+
+    /**
+     *
+     */
+    public boolean shuffleTiles(){
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public boolean exchangeTile(){
+        return false;
+
+    }
+
+    /**
+     *
+     */
+    public boolean selectBlankTileLetter(){
+
+        return false;
+    }
 }
--- app/src/main/java/com/example/scrabble_gamestate/Tile.java
+++ app/src/main/java/com/example/scrabble_gamestate/Tile.java
@@ -1,4 +1,19 @@
 package com.example.scrabble_gamestate;
 
 public class Tile {
+
+    //instance vars
+    int pointVal;
+    char tileLetter; //for blank tiles, use a single space
+    int androidId;
+
+    /**
+     * Constructor for Tile class
+     */
+    public Tile(int points, char letter, int id)
+    {
+        pointVal = points;
+        tileLetter = letter;
+        androidId = id;
+    }
 }
