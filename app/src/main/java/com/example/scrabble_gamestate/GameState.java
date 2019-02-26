0035f009fb81a5d55642ca0ce172483d3bcf7215
Sarah <bunger20@up.edu>
2019-02-25 18:51:47 -0800

    Added in onClick method skeleton (not actual method)

--- app/src/main/java/com/example/scrabble_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble_gamestate/GameState.java
@@ -86,154 +86,154 @@ public class GameState {
         ///////////////////////////to be added!
 
         //a tiles
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_2));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_3));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_4));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_5));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_6));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_7));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_8));
-        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_9));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_2, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_3, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_4, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_5, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_6, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_7, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_8, false));
+        tileBag.add(new Tile(A_VAL, 'a', R.drawable.tile_a_9, false));
 
         //b tiles
-        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b));
-        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b_2));
+        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b, false));
+        tileBag.add(new Tile(B_VAL, 'b', R.drawable.tile_b_2, false));
 
         //c tiles
-        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c));
-        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c_2));
+        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c, false));
+        tileBag.add(new Tile(C_VAL, 'c', R.drawable.tile_c_2, false));
 
         //d tiles
-        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d));
-        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_2));
-        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_3));
-        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_4));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d, false));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_2, false));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_3, false));
+        tileBag.add(new Tile(D_VAL, 'd', R.drawable.tile_d_4, false));
 
         //e tiles
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_2));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_3));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_4));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_5));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_6));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_7));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_8));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_9));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_10));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_11));
-        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_12));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_2, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_3, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_4, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_5, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_6, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_7, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_8, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_9, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_10, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_11, false));
+        tileBag.add(new Tile(E_VAL, 'e', R.drawable.tile_e_12, false));
 
         //f tiles
-        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f));
-        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f_2));
+        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f, false));
+        tileBag.add(new Tile(F_VAL, 'f', R.drawable.tile_f_2, false));
 
         //g tiles
-        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g));
-        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_2));
-        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_3));
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g, false));
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_2, false));
+        tileBag.add(new Tile(G_VAL, 'g', R.drawable.tile_g_3, false));
 
         //h tiles
-        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h));
-        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h_2));
+        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h, false));
+        tileBag.add(new Tile(H_VAL, 'h', R.drawable.tile_h_2, false));
 
         //i tiles
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_2));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_3));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_4));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_5));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_6));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_7));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_8));
-        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_9));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_2, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_3, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_4, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_5, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_6, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_7, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_8, false));
+        tileBag.add(new Tile(I_VAL, 'i', R.drawable.tile_i_9, false));
 
         //j tile
-        tileBag.add(new Tile(J_VAL, 'j', R.drawable.tile_j));
+        tileBag.add(new Tile(J_VAL, 'j', R.drawable.tile_j, false));
 
         //k tile
-        tileBag.add(new Tile(K_VAL, 'k', R.drawable.tile_k));
+        tileBag.add(new Tile(K_VAL, 'k', R.drawable.tile_k, false));
 
         //l tiles
-        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l));
-        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_2));
-        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_3));
-        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_4));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l, false));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_2, false));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_3, false));
+        tileBag.add(new Tile(L_VAL, 'l', R.drawable.tile_l_4, false));
 
         //m tiles
-        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m));
-        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m_2));
+        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m, false));
+        tileBag.add(new Tile(M_VAL, 'm', R.drawable.tile_m_2, false));
 
         //n tiles
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n));
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_2));
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_3));
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_4));
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_5));
-        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_6));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n, false));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_2, false));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_3, false));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_4, false));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_5, false));
+        tileBag.add(new Tile(N_VAL, 'n', R.drawable.tile_n_6, false));
 
         //o tiles
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_2));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_3));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_4));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_5));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_6));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_7));
-        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_8));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_2, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_3, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_4, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_5, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_6, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_7, false));
+        tileBag.add(new Tile(O_VAL, 'o', R.drawable.tile_o_8, false));
 
         //p tiles
-        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p));
-        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p_2));
+        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p, false));
+        tileBag.add(new Tile(P_VAL, 'p', R.drawable.tile_p_2, false));
 
         //q tile
-        tileBag.add(new Tile(Q_VAL, 'q', R.drawable.tile_q));
+        tileBag.add(new Tile(Q_VAL, 'q', R.drawable.tile_q, false));
 
         //r tiles
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r));
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_2));
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_3));
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_4));
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_5));
-        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_6));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r, false));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_2, false));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_3, false));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_4, false));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_5, false));
+        tileBag.add(new Tile(R_VAL, 'r', R.drawable.tile_r_6, false));
 
         //s tiles
-        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s));
-        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_2));
-        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_3));
-        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_4));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s, false));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_2, false));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_3, false));
+        tileBag.add(new Tile(S_VAL, 's', R.drawable.tile_s_4, false));
 
         //t tiles
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t));
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_2));
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_3));
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_4));
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_5));
-        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_6));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t, false));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_2, false));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_3, false));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_4, false));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_5, false));
+        tileBag.add(new Tile(T_VAL, 't', R.drawable.tile_t_6, false));
 
         //u tiles
-        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u));
-        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_2));
-        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_3));
-        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_4));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u, false));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_2, false));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_3, false));
+        tileBag.add(new Tile(U_VAL, 'u', R.drawable.tile_u_4, false));
 
         //v tiles
-        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v));
-        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v_2));
+        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v, false));
+        tileBag.add(new Tile(V_VAL, 'v', R.drawable.tile_v_2, false));
 
         //w tiles
-        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w));
-        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w_2));
+        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w, false));
+        tileBag.add(new Tile(W_VAL, 'w', R.drawable.tile_w_2, false));
 
         //x tile
-        tileBag.add(new Tile(X_VAL, 'x', R.drawable.tile_x));
+        tileBag.add(new Tile(X_VAL, 'x', R.drawable.tile_x, false));
 
         //y tiles
-        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y));
-        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y_2));
+        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y, false));
+        tileBag.add(new Tile(Y_VAL, 'y', R.drawable.tile_y_2, false));
 
         //z tile
-        tileBag.add(new Tile(Z_VAL, 'z', R.drawable.tile_z));
+        tileBag.add(new Tile(Z_VAL, 'z', R.drawable.tile_z, false));
 
     }//makeTileBag
 
