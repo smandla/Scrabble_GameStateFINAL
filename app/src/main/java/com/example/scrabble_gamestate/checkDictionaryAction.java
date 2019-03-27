68920a783cab760eafc3fa649c742b118cb1c9d4
Kavya Mandla <mandla21@up.edu>
2019-03-27 10:56:35 -0700

    added change based on HW4d feedback

--- app/src/main/java/com/example/scrabble_gamestate/checkDictionaryAction.java
+++ app/src/main/java/com/example/scrabble_gamestate/checkDictionaryAction.java
@@ -1,12 +1,4 @@
 package com.example.scrabble_gamestate;
-/**
- *does dictionary stuff
- *
- * @author Sydney Wells
- * @author Sarah Bunger
- * @author Kavva Mandla
- * @author Meredith Marcinko
- * @version February 2019
- */
-public class checkDictionaryAction {
+
+public class CheckDictionaryAction {
 }
