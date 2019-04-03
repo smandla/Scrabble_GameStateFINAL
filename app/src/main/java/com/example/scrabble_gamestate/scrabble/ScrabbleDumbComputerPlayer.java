74234a82472231330d138674afd9c2b0309f67af
Kavya Mandla <mandla21@up.edu>
2019-04-02 19:38:06 -0700

    added some changes to dumb ai

--- app/src/main/java/com/example/scrabble_gamestate/scrabble/ScrabbleDumbComputerPlayer.java
+++ app/src/main/java/com/example/scrabble_gamestate/scrabble/ScrabbleDumbComputerPlayer.java
@@ -6,7 +6,6 @@ import android.widget.Switch;
 
 import com.example.scrabble_gamestate.game.GameComputerPlayer;
 import com.example.scrabble_gamestate.game.infoMsg.GameInfo;
-import com.example.scrabble_gamestate.game.
 import com.example.scrabble_gamestate.game.util.Tickable;
 import com.example.scrabble_gamestate.game.Tile;
 import com.example.scrabble_gamestate.scrabble.ScrabbleGameState;
@@ -26,19 +25,20 @@ import java.util.ArrayList;
  * @version September 2013
  */
 public class ScrabbleDumbComputerPlayer extends GameComputerPlayer implements Tickable {
-    ArrayList<String> letters;
+
+        ArrayList<String> letters;
     String alreadyPlayedLetter = null;
     Tile alreadyPlayedTile = null;
     boolean foundWord = false;
+
     // String word = read in line from dumbDictionary.txt
     String word = " ";
-    ScrabbleGameState latestState =  new ScrabbleGameState();
+    ScrabbleGameState latestState = new ScrabbleGameState();
+
     /**
-     *
      * Constructor for objects of class CounterComputerPlayer1
      *
-     * @param name
-     * 		the player's name
+     * @param name the player's name
      */
     public ScrabbleDumbComputerPlayer(String name) {
         // invoke superclass constructor
@@ -46,44 +46,41 @@ public class ScrabbleDumbComputerPlayer extends GameComputerPlayer implements Ti
 
         // start the timer, ticking 20 times per second
         getTimer().setInterval(50);
-        getTimer().start();
+        getTimer().start()
     }
+
 
     /**
      * callback method--game's state has changed
      *
-     * @param info
-     * 		the information (presumably containing the game's state)
+     * @param info the information (presumably containing the game's state)
      */
     @Override
     protected void receiveInfo(GameInfo info) {
-        if (info instanceof ScrabbleGameState){
-            this.latestState = (ScrabbleGameState)info;
-        }
-        else {
+        if (info instanceof ScrabbleGameState) {
+            this.latestState = (ScrabbleGameState) info;
+        } else {
             //TODO: should do more here?
             return;
         }
 
         //half the time, just skip turn instead of playing a word
-        if (Math.random() >= 0.5){
+        if (Math.random() >= 0.5) {
             game.sendAction(new SkipTurnAction(this));
             return;
         }
 
         //the other half of the time, play a word
         //TODO: implement algorithm
-        for(int column = 1; column < 14; column++){
-            for(int row = 0; row < 15; row++)
-            {
-                if((latestState.getBoard()[row][column] == null) &&
+        for (int column = 1; column < 14; column++) {
+            for (int row = 0; row < 15; row++) {
+                if ((latestState.getBoard()[row][column] == null) &&
                         (latestState.getBoard()[row - 1][column] == null) &&
-                        (latestState.getBoard()[row+1][column] == null)){
-                    if(alreadyPlayedLetter != null)
-                    {
-                        for (Tile t: hand) {
-                            if(letters.contains(t.getTileLetter()) == false){
-                                //TODO: ADD.() DOES NOT ACCEPT CHAR!!! NEED STRING
+                        (latestState.getBoard()[row + 1][column] == null)) {
+                    if (alreadyPlayedLetter != null) {
+                        for (Tile t : latestState.getHand2() ) {
+                            if (letters.contains(t.getTileLetter()) == false) {
+
                                 letters.add(t.getTileLetter());
                             }
 
@@ -93,63 +90,56 @@ public class ScrabbleDumbComputerPlayer extends GameComputerPlayer implements Ti
                          * use a switch statement to determine which line number constant to use
                          *             (example: if additionalLetter = = ‘a’ use line number constant for A)
                          */
-                        while(foundWord == false){
-                            if(/**TODO: if it's still AI's turn)**/){
-                                //TODO: ask NUXOLL about InputStream
-                                //use scanner & print writer
+                        /**while(foundWord == false){
+                         if(TODO: if it's still AI's turn)){
+                         TODO: ask NUXOLL about InputStream
+                         use scanner & print writer
+                         }**/
 
-
-
+                    }
+                    for (int i = 0; i < word.length(); i++) {
+                        String sub = word.substring(i);
+                        int numFound = 0;
+                        for (String s : letters) {
+                            if ((sub.equals(s) == true) && (s.equals(alreadyPlayedLetter)) == false) {
+                                numFound++;
                             }
 
                         }
-                        for(int i = 0; i < word.length(); i++){
-                            String sub = word.substring(i);
-                            int numFound = 0;
-                            for (String s: letters) {
-                                if((sub.equals(s) == true) && (s.equals(alreadyPlayedLetter)) == false){
-                                    numFound++;
-                                }
+                        if (numFound > word.length() - 1) {
+                            foundWord = true;
+                            int alreadyPlayedX = alreadyPlayedTile.getxCoord();
+                            int alreadyPlayedY = alreadyPlayedTile.getyCoord();
 
-                            }
-                            if(numFound > word.length() - 1){
-                                foundWord = true;
-                                int alreadyPlayedX = alreadyPlayedTile.getxCoord();
-                                int alreadyPlayedY = alreadyPlayedTile.getyCoord();
-                                
-                                for(int j = word.length(); j > 0; j++){
-                                    for (Tile t: hand) {
-                                        //TODO: THIS MAY OR MAY NOT WORK
-                                        if(tileLetter == word.charAt(j) && latestState.getBoard()[alreadyPlayedX -i][alreadyPlayedY] == null){
-                                            placeTile();
-                                        }
-                                        
+                            for (int j = word.length(); j > 0; j++) {
+                                for (Tile t : latestState.getHand2()) {
+                                    //TODO: THIS MAY OR MAY NOT WORK
+                                    if ((tileLetter == word.charAt(j)) && latestState.getBoard()[alreadyPlayedX - i][alreadyPlayedY] == null) {
+                                        placeTile();
                                     }
-                                    playWord(AI's turnID)
+
                                 }
+                                playWord(AI 's turnID)
                             }
                         }
                     }
                 }
-                //TODO: fix this
-                if(board[row][column] != null && board[row-1][column] == null && board[row+1][column] == null){
-                    additionalLetter = //value of Tile in row;
-                    alreadyPlayed = //Tile in row;
-                }
-
             }
+            //TODO: fix this
+            if (latestState.getBoard()[row][column] != null && latestState.getBoard()[row - 1][column] == null && latestState.getBoard()[row + 1][column] == null) {
+                additionalLetter = //value of Tile in row;
+                        alreadyPlayed = //Tile in row;
+            }
+
         }
-
     }
-
 /**
  * TODO: ALL DIS
- * if we went through the entire board and didn't find a place to play the word still,
+* if we went through the entire board and didn't find a place to play the word still,
  * the AI will have to skip its turn
- * if(foundWord == false){
- *         use skipTurn() method
+ * if(foundWord == false)// * use skipTurn() method
  */
-
-
 }
+
+
 
